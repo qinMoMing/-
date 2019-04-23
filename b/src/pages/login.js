953f30ee360290cdbfdea7bf6844b79ffdf4baed
moming <you@example.com>
2019-04-23 23:34:36 +0800

    个人中心

--- b/src/pages/login.js
+++ b/src/pages/login.js
@@ -246,21 +246,14 @@ class Login extends React.Component {
           )}
         </Form.Item> */}
         <Form.Item
-          label="验证码"
+          label="手机验证码"
           extra="We must make sure that your are a human."
         >
-          <Row gutter={8}>
-            <Col span={12}>
-              {getFieldDecorator('captcha', {
-                rules: [{ required: true, message: 'Please input the captcha you got!' }],
-              })(
-                <Input />
-              )}
-            </Col>
-            <Col span={12}>
-              <Button>Get captcha</Button>
-            </Col>
-          </Row>
+            {getFieldDecorator('captcha', {
+              rules: [{ required: true, message: 'Please input the captcha you got!' }],
+            })(
+              <Input />
+            )}
         </Form.Item>
         <Form.Item {...tailFormItemLayout}>
           {getFieldDecorator('agreement', {
@@ -280,24 +273,84 @@ class Login extends React.Component {
       </Form>
     )
   }
-
+  renderResetPassword = () => {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    }; 
+    return(
+      <Form {...formItemLayout} onSubmit={this.handleSubmit} className={styles.wrapForm}>
+        <Form.Item
+          label="旧密码"
+        >
+          {getFieldDecorator('oldPassword', {
+            rules: [{
+              required: true, message: 'Please input your password!',
+            }, {
+              validator: this.validateToNextPassword,
+            }],
+          })(
+            <Input type="password" />
+          )}
+        </Form.Item>
+        <Form.Item
+          label="输入新密码"
+        >
+          {getFieldDecorator('newPassword', {
+            rules: [{
+              required: true, message: 'Please input your password!',
+            }, {
+              validator: this.validateToNextPassword,
+            }],
+          })(
+            <Input type="password" />
+          )}
+        </Form.Item>
+        <Form.Item
+          label="再次输入新密码"
+        >
+          {getFieldDecorator('confirm', {
+            rules: [{
+              required: true, message: 'Please confirm your password!',
+            }, {
+              validator: this.compareToFirstPassword,
+            }],
+          })(
+            <Input type="password" onBlur={this.handleConfirmBlur} />
+          )}
+        </Form.Item>
+      </Form>
+    )
+  }
   renderCenter = () => {
     return(
-      <div style={{width:"156px",padding:"100px 0"}}>
+      <div style={{width:"186px",padding:"64px 0"}}>
         <Menu
           defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
           mode="inline"
           theme="dark"
         >
           <Menu.Item key="1">
-            <span>Option 1</span>
+            <span>个人信息管理</span>
           </Menu.Item>
           <Menu.Item key="2">
-            <span>Option 2</span>
+            <span>平台密码修改</span>
           </Menu.Item>
           <Menu.Item key="3">
-            <span>Option 3</span>
+            <span>爱心币管理</span>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <span>我发起项目的管理</span>
+          </Menu.Item>
+          <Menu.Item key="5">
+            <span>我参与项目的管理</span>
           </Menu.Item>
         </Menu>
       </div>
@@ -315,63 +368,5 @@ class Login extends React.Component {
   }
 }
 export default Login;
-{/*import { Menu, Icon, Button } from 'antd';
-
-const SubMenu = Menu.SubMenu;
-
-class App extends React.Component {
-  state = {
-    collapsed: false,
-  }
-
-  toggleCollapsed = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  }
-
-  render() {
-    return (
-      <div style={{ width: 256 }}>
-        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-          <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
-        </Button>
-        <Menu
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode="inline"
-          theme="dark"
-          inlineCollapsed={this.state.collapsed}
-        >
-          <Menu.Item key="1">
-            <Icon type="pie-chart" />
-            <span>Option 1</span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Icon type="desktop" />
-            <span>Option 2</span>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="inbox" />
-            <span>Option 3</span>
-          </Menu.Item>
-          <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-            <Menu.Item key="5">Option 5</Menu.Item>
-            <Menu.Item key="6">Option 6</Menu.Item>
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu>
-          </SubMenu>
-        </Menu>
-      </div>
-    );
-  }
-}
+{/*
 */} 
